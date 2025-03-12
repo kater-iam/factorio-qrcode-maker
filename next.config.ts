8aec03de0fd799b4b7ae649571c2439178d502fa
Yousan_O <yousan@l2tp.org>
2025-03-12 10:13:04 +0900

    GitHub Pagesへのデプロイのための設定を追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/factorio-qrcode-maker' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/factorio-qrcode-maker/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
